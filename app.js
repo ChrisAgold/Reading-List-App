b9f2616cdc30616dadf233398e0ed6fa014c563f
Chris Agold <christophersagold@gmail.com>
2020-12-23 08:12:01 -0600

    added hide button

--- app.js
+++ app.js
@@ -251,7 +251,32 @@ addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
     console.log(value);
+
+    // create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li)
 });
+
+// Lesson #15
+// Hide Books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener("change", function(e){
+    if(hideBox.checked){
+        list.style.display="none";
+    } else {
+        list.style.display = "initial";
+    }
+})
 
 
 
